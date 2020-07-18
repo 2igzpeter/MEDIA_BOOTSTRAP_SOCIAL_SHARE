478deadcc0629c61ed3aebf57ce16aa3139acd3b
2igzpeter <monsieur.aveline@gmail.com>
2020-07-18 18:07:27 +0200

    ajout de code dans les fichiers: database.js/journal.js/passport.js

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+ 'connection':{
+  'host':'localhost',
+  'user':'root',
+  'password':''
+ },
+ 'database':'nodejs_login',
+ 'user_table':'users',
+ 'viewpost_table': 'viewpost'
+}
+
+console.log('./database.js is ok')
---
+++ config/journal.js
@@ -0,0 +1,21 @@
+
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodejs_login"
+   });
+
+
+
+module.exports = function(app) {
+
+
+};
+
+
+
+   console.log('./journal.js is ok')
